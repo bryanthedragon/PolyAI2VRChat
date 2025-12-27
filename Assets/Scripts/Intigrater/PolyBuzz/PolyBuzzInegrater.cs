e033bdb17a53f48621773900987bff4beb4611b4
BryanTheDragon <107201903+bryanthedragon@users.noreply.github.com>
2025-12-26 19:25:44 -0600

    BASE

---
+++ Assets/Scripts/Intigrater/PolyBuzz/PolyBuzzInegrater.cs
@@ -0,0 +1,19 @@
+using PolyBuzzToVRChat.Accounting.PolyBuzz;
+
+namespace PolyBuzzToVRChat.Integrator.PolyBuzz
+{
+    public class PolyBuzzIntegrator
+    {
+        public void Integrate()
+        {
+            // Integration logic goes here
+        }
+        class Account : PolybuzzAccount
+        {
+            public void GetDisplayInfo() 
+            {
+                DisplayInfo();
+            }
+        }
+    }
+}
